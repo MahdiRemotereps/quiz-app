4e11e0d1af27e359c410e54effd111d031b9a4d4
Mahdi Zarrinkolah <mahdi@remotereps.com>
2023-07-11 13:57:52 +0330

    initial

---
+++ src/icons/index.js
@@ -0,0 +1,5 @@
+export { default as CheckCircleIcon } from "./CheckCircleIcon";
+export { default as XMarkIcon } from "./XMarkIcon";
+export { default as ExclamationIcon } from "./ExclamationIcon";
+export { default as HomeIcon } from "./HomeIcon";
+export { default as XCircleIcon } from "./XCircleIcon";
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#fefce8",
+          100: "#fef9c3",
+          200: "#fef08a",
+          300: "#fde047",
+          400: "#facc15",
+          500: "#eab308",
+          600: "#ca8a04",
+          700: "#a16207",
+          800: "#854d0e",
+          900: "#713f12",
+          950: "#422006",
+        },
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
